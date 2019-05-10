9a0ec213aba42fc4819ed3caf565900f225b0b39
Thiago Capelo <thiagokpelo@gmail.com>
2019-05-10 09:51:01 -0300

    Gatsby Netlify Form - Tutorial

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -25,42 +25,6 @@ const Button = styled.button`
   }
 `
 
-var serialize = function(form: any) {
-  // Setup our serialized data
-  var serialized = []
-
-  // Loop through each field in the form
-  for (var i = 0; i < form.elements.length; i++) {
-    var field = form.elements[i]
-
-    // Don't serialize fields without a name, submits, buttons, file and reset inputs, and disabled fields
-    if (
-      !field.name ||
-      field.disabled ||
-      field.type === 'file' ||
-      field.type === 'reset' ||
-      field.type === 'submit' ||
-      field.type === 'button'
-    )
-      continue
-
-    // If a multi-select, get all selections
-    if (field.type === 'select-multiple') {
-      for (var n = 0; n < field.options.length; n++) {
-        if (!field.options[n].selected) continue
-        serialized.push(encodeURIComponent(field.name) + '=' + encodeURIComponent(field.options[n].value))
-      }
-    }
-
-    // Convert field data to a query string
-    else if ((field.type !== 'checkbox' && field.type !== 'radio') || field.checked) {
-      serialized.push(encodeURIComponent(field.name) + '=' + encodeURIComponent(field.value))
-    }
-  }
-
-  return serialized.join('&')
-}
-
 export const Form: React.FC<any> = () => {
   const handleSubmit = (evt: React.FormEvent<any>) => {
     evt.preventDefault()
@@ -70,7 +34,8 @@ export const Form: React.FC<any> = () => {
   }
 
   return (
-    <FormComponent id="contact-form" name="contact-form" method="POST" action="/contact-sent">
+    <FormComponent id="contact-form" name="contact-form" method="POST" data-netlify-honeypot="bot-field" action="/contact-sent">
+      <input type="hidden" name="form-name" value="contact-form" />
       <TextField name="name" type="text" placeholder="Name" required />
       <TextField name="email" type="email" placeholder="Email" required />
       <TextField name="message" placeholder="Message..." as="textarea" rows={15} required />
